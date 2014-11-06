0a8bd82e04fc5b2c00ff4baf0b45ab4b52171add
blogscot <iain.diamond@gmail.com>
2014-11-06 11:41:51 +0000

    Invaders now move down the screen.

--- src/com/diamond/iain/javagame/entities/Aliens.java
+++ src/com/diamond/iain/javagame/entities/Aliens.java
@@ -9,18 +9,19 @@ import java.util.ArrayList;
 
 import com.diamond.iain.javagame.gfx.SpriteManager;
 
-
 public class Aliens {
-	
+
 	ArrayList<Invader> invaders = new ArrayList<>();
-	Point anchor = new Point(30,50);
+	Point anchor = new Point(30, 50);
 	private final int xSpacing = 35;
 	private final int ySpacing = 85;
 
-	public Aliens(SpriteManager manager){
-		
+	boolean hitWall = false;
+
+	public Aliens(SpriteManager manager) {
+
 		// Build me an army
-		
+
 		// Row 1
 		Invader m1_1 = new Martian(manager, anchor);
 		anchor.translate(xSpacing, 0);
@@ -31,24 +32,30 @@ public class Aliens {
 		Invader m2_1 = new Martian(manager, anchor);
 		anchor.translate(xSpacing, 0);
 		Invader m2_2 = new Martian(manager, anchor);
-		
+
 		invaders.add(m1_1);
 		invaders.add(m1_2);
 		invaders.add(m2_1);
 		invaders.add(m2_2);
 	}
-	
+
 	public void tick() {
-		
+
 		// check if any invader has hit the left or right wall
-		for (Invader invader : invaders){
+		for (Invader invader : invaders) {
 			double pos = invader.getPosition().getX();
-			if ( pos > RightWall || pos < LeftWall){
+			if (pos > RightWall || pos < LeftWall) {
 				invader.reverseDirection();
+				hitWall = true;
 				break;
 			}
 		}
-		
+
+		if (true == hitWall) {
+			hitWall = false;
+			invaders.stream().forEach(Invader::moveDown);
+		}
+
 		invaders.stream().forEach(Invader::tick);
 	}
 
